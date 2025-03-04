8fc1cd9f09542ed05da6400458b9c24e5adcb55d
cprpython25 <cpellico79@gmail.com>
2025-03-04 19:47:11 +0100

    first commit

---
+++ Data/python01personas.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+print("primer ejemplo DataFrame)
+      
+#vamos a crear un diccionario con elementos que se llaman SERIES
+#no deja de ser un diccionario con valores que van correspondientes entre
+#ellos, aunque podrían no ser correspondientes.
+datos = {
+    'nombres': '[Lucía', 'Andrea', 'Alex', 'Antonia"]
+    ,'edad'
+}
